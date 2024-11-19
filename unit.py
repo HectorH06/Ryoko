d974610a6af006c90da101676584f583e5af080a
eE06 <hector8heredia@gmail.com>
2024-11-19 17:33:34 -0600

    Unit request

--- unit.py
+++ unit.py
@@ -7,6 +7,8 @@ from scraper import run
 orig = "Guadalajara"
 dest = "Tokyo"
 
+# Search a single flight
+
 sched = [orig, dest, "03-09-2025", "03-16-2025"]
 with sync_playwright() as playwright:
             flight_data = run(sched, playwright)
